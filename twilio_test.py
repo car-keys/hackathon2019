8f45b236238be6781a6514a0c46f9a01275fc261
Nick Engle <enick11@gmail.com>
2019-03-03 08:44:25 -0500

    cleanup and config file

--- twilio_test.py
+++ twilio_test.py
@@ -2,22 +2,27 @@ import time
 from twilio.rest import Client
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
-log_file = "/var/log/suricata/fast.log"
-key_file = "key.txt"
-# client = Client(account_sid, auth_token)
 
+# client = Client(account_sid, auth_token)
 # message = client.messages.create(
     # to="=19374896142",
     # from_="+15138153419",
     # body="snek")    
 
-account_sid = "AC5ec563f144a1a2dc448822e01fffd13f"
+# read config file
+with open("config.json", "r") as f:
+    config = json.load(f)
+log_file = config["suricata_log_file_full_path"]
+key_file = config["suricata_api_key_file"]
+account_sid = config["twilio_account_sid"]
+account_number = config["twilio_account_phone_number"]
+test_recipient_number = config["twilio_recipient_number"]
+
+# grab api token. kept seperate so its not included in the repo
 with open(key_file, "r") as f:
     auth_token = str(f.read())
-account_number = "+15138153419"
-test_recipient_number = "15132771334"
 
-
+# called when file observer notices a file change.
 class FileModifiedEventHandler(FileSystemEventHandler):
     def on_modified(self, event):
         print('Event!')
@@ -37,7 +42,8 @@ class TextSender(Client):
             from_=self.account_phone,
             body=message)
         return messageid
-        
+
+# called when log file changes        
 def handle_file_change(event):
     print(event.src_path)
     # sender = TextSender(account_sid, auth_token, account_number)
@@ -45,12 +51,14 @@ def handle_file_change(event):
     # with open(log_file, 'r') as f:
     #    newline = f.readlines()[-1]
 
-
+# set up watchdog event observer
 event_handler = FileModifiedEventHandler()
 observed_path = "/var/log/suricata/"
 observer = Observer()
 observer.schedule(event_handler, observed_path)
 observer.start()
+
+# loop until Ctrl+C to quit
 try:
     while True:
         print('sleeping')
